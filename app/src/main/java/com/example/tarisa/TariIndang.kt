02c605cc5285705a857efde89eea4c8b5d68804a
tiarafadhila <tiarafadhila2002@gmail.com>
2023-12-28 18:49:58 +0700

    Aplikasi Tarisa

--- app/src/main/java/com/example/tarisa/TariIndang.kt
+++ app/src/main/java/com/example/tarisa/TariIndang.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class TariIndang : AppCompatActivity() {
-    private lateinit var Button_back : Button
+    private lateinit var Button_back : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tari_indang)
 
-        Button_back = findViewById(R.id.button_back11)
+        Button_back = findViewById(R.id.button_back111)
 
         Button_back.setOnClickListener{
             val move = Intent (this@TariIndang, SumateraBarat::class.java)
